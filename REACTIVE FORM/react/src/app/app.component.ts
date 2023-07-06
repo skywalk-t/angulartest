bcd8bc07c28f8a96b0ddbfcbc74c0001cb4ea19b
Chinchuprasanth <chinchuprasanth555@gmail.com>
2023-07-06 08:28:36 +0530

    new

---
+++ REACTIVE FORM/react/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormBuilder,FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'react';
+get alternativeEmails() {
+  return this.registration.get('alternativeEmails') as FormArray
+}
+
+addalternativeEmail(){
+  console.log('alternativeEmails',this.alternativeEmails)
+  this.alternativeEmails.push(this.fb.control(''))
+  console.log(this.fb.control(''))
+}
+
+  
+  constructor(private fb:FormBuilder) { }
+registration = this.fb.group({
+  email:['Chinchu'],
+  alternativeEmails:this.fb.array([])
+})
+
+onSubmit(values:any){
+console.log(values.value)
+}
+}
