61d25c0418fda9c0cfac3e4680b60cb3751b87c4
Chinchuprasanth <chinchuprasanth555@gmail.com>
2023-07-10 02:24:39 +0530

    assighnment

--- Assighnment Form/reg/src/app/app.component.ts
+++ Assighnment Form/reg/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder,FormArray, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reg';
+  public isChecked = true;
+
+  get f() { 
+    return this.register.controls
+  }
+
+
+
+  submit=false
+  get alternativeEmails(){
+    return this.register.get('alternativeEmails') as FormArray
+  }
+
+  constructor (private fb:FormBuilder) { }
+
+register=this.fb.group({
+  firstname:['',Validators.required],
+  lastname:['',Validators.required], 
+  phone:['',[Validators.required,Validators.pattern('((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,30})')]], 
+  email:['',[Validators.required,Validators.email]],
+  address:['',Validators.required],
+  about:['',Validators.required],
+
+alternativeEmails:this.fb.array([])
+})
+ 
+addalternativeEmail() {
+  console.log('alternativeEmails',this.alternativeEmails)
+  this.alternativeEmails.push(this.fb.control(''))
+  console.log(this.fb.control(''))
+}
+
+onsubmit(values:any){
+  console.log(values.value)
+  this.submit=true
+  console.log('clicked')
+
+}
+
 }
