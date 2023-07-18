00f1945e946703920bb8abbb9d7f7af29b5c3a18
sumesh.pu <sumesh.unnikrishnan@securitybenefit.com>
2023-07-18 09:22:41 -0700

    change in forncontrolgroup

--- Demo App/demo/src/app/form/form.component.ts
+++ Demo App/demo/src/app/form/form.component.ts
@@ -12,26 +12,30 @@ export class FormComponent {
 
   register = new FormGroup({
 
-firstname : new FormControl('',Validators.required),
-lastname : new FormControl('',Validators.required),
-email : new FormControl('',Validators.required),
-gender : new FormControl('',Validators.required),
-ismarried : new FormControl('',Validators.required),
-country : new FormControl('',Validators.required),
-address: new FormGroup({
-  city: new FormControl('',Validators.required),
-  street: new FormControl('',Validators.required),
-  pincode: new FormControl('',Validators.required)
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    gender: new FormControl('', Validators.required),
+    ismarried: new FormControl('', Validators.required),
+    country: new FormControl('', Validators.required),
+    address: new FormGroup({
+      city: new FormControl('', Validators.required),
+      street: new FormControl('', Validators.required),
+      pincode: new FormControl('', Validators.required)
 
-})
+    })
 
   })
 
-  get f() { 
-    return this.register.controls; 
-    
+  get f() {
+    return this.register.controls;
+
   }
- 
+  get address() {
+    return this.register.controls.address.controls;
+
+  }
+
 
   onsubmit() {
     console.log(this.register.value)
