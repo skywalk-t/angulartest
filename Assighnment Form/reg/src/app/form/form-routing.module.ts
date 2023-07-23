ba3a0c891506cef7dc205c9de34a66e7c02b22ee
Chinchuprasanth <chinchuprasanth555@gmail.com>
2023-07-24 03:23:51 +0530

    path matching error

--- Assighnment Form/reg/src/app/form/form-routing.module.ts
+++ Assighnment Form/reg/src/app/form/form-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddComponent } from './add/add.component';
 
 const routes: Routes = [
-  {path:'', component: AddComponent},
+  {path:'', component: AddComponent,pathMatch:'full'},
   
 ];
 
