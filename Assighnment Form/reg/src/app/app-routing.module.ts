db1594bfeadee014e1cbbe50e7511c4f8ec387f9
Chinchuprasanth <chinchuprasanth555@gmail.com>
2023-07-24 04:05:14 +0530

    home component

--- Assighnment Form/reg/src/app/app-routing.module.ts
+++ Assighnment Form/reg/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormModule } from './form/form.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:'Home', component:AppComponent},
+  {path:'Home', component:HomeComponent},
   {path:'',redirectTo:'app', pathMatch: 'full'},
   {path:'add',loadChildren: ()=>import('./form/form.module').then(m=> m.FormModule)}
 ];
