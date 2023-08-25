6c53588526db90b0ac0583cd950efbc25bb86f58
Chinchuprasanth <chinchuprasanth555@gmail.com>
2023-08-25 07:13:29 +0530

    new components

--- Service/service/src/app/app-routing.module.ts
+++ Service/service/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cmp1', component: Component1Component },
+  { path: 'cmp2', component: Component2Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
